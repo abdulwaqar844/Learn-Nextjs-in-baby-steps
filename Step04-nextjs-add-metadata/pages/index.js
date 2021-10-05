08147e33a7c7564e4ad54d69a5574d174ffcd776
abdulwaqar844 <abdulwaqar844@gmail.com>
2021-10-05 23:23:11 +0500

    added step 04

---
+++ Step04-nextjs-add-metadata/pages/index.js
@@ -0,0 +1,17 @@
+import Link from "next/link";
+import Head from 'next/head'
+import Profile from "./../components/profile";
+export default function Home() {
+  return (
+    <div>
+        <Head>
+      <title>Welcome To Nextjs</title>
+    </Head>
+      <Profile />
+      Hello World.
+      <Link href="/about">
+        <a>About</a>
+      </Link>
+    </div>
+  );
+}
