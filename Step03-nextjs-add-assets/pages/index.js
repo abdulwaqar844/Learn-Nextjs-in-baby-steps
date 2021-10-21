e7d41a6dd3be5838d7fc43be13cae268f2b115f7
Abdul Waqar <abdulwaqar844@gmail.com>
2021-10-21 10:16:05 +0000

    style updated

--- Step03-nextjs-add-assets/pages/index.js
+++ Step03-nextjs-add-assets/pages/index.js
@@ -2,9 +2,10 @@ import Link from "next/link";
 import Profile from "./../components/profile";
 export default function Home() {
   return (
-    <div>
-      <Profile />
-      Hello World.
+    <div style={{display:'flex',flexDirection:'column-reverse'}}>
+    <div>  <Profile /></div>
+     <hr/>
+      <h2>Hello World.</h2>
       <Link href="/about">
         <a>About</a>
       </Link>
