558f3db05e2c8d62434a62f6ef31affb77316a38
Abdul Waqar <abdulwaqar844@gmail.com>
2021-10-21 10:48:53 +0000

    updated step06 updated data api

--- Step06-nextjs-data-fetching-with-getStaticProps-at-build-time/pages/index.js
+++ Step06-nextjs-data-fetching-with-getStaticProps-at-build-time/pages/index.js
@@ -1,23 +1,29 @@
-import Link from "next/link";
-import Head from 'next/head'
-import Profile from "../components/profile";
-export default function Home() {
-
+// import Link from "next/link";
+import Head from "next/head";
+export default function Home({ posts }) {
   return (
     <div>
-        <Head>
-      <title>Welcome To Nextjs</title>
-    </Head>
-      <Profile />
-      Hello World.
-      <Link href="/about">
-        <a>About</a>
-      </Link>
+      <Head>
+        <title>Welcome To Nextjs</title>
+      </Head>
+      <div>
+        <h2>All posts</h2>
+        {posts.map((post) => {
+          return (
+            <div key={post.userId}>
+              <h2>{post.title}</h2>
+              <p>{post.body}</p>
+            </div>
+          );
+        })}
+      </div>
     </div>
   );
 }
-async export default function getStaticprops(){
-  const res= await fetch('https://jsonplaceholder.typicode.com/posts')
-  const data=await res.json()
-  console.log(data)
+export async function getStaticProps(context) {
+  const res = await fetch("https://jsonplaceholder.typicode.com/posts");
+  const posts = await res.json();
+  return {
+    props: { posts }, // will be passed to the page component as props
+  };
 }
